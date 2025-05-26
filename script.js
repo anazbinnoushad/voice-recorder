dd1032bf9985d1d4e489b49b49b519988485b9ff
Anaz N <anazbinnoushad@gmail.com>
2025-05-27 02:14:53 +0530

    Add basic waveform to record button

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let recordButton = document.getElementById('recordButton');
 let recordingsList = document.getElementById('recordingsList');
 let recordTemplate = document.getElementById('recordTemplate');
 
+let canvas = document.getElementById('waveformCanvas');
+let canvasCtx = canvas.getContext('2d');
+let animationId;
+
 let mediaRecorder;
 let audioChunks = [];
 let audioContext, analyser, source;
@@ -14,6 +18,57 @@ recordButton.onclick = async () => {
         stopRecording();
     }
 };
+
+function drawWaveform() {
+    const canvas = document.getElementById('waveformCanvas');
+    const ctx = canvas.getContext('2d');
+
+    const rect = canvas.getBoundingClientRect();
+    canvas.width = rect.width;
+    canvas.height = rect.height;
+
+    analyser.fftSize = 256;
+    analyser.smoothingTimeConstant = 0.8;
+
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+
+    function draw() {
+        animationId = requestAnimationFrame(draw);
+        analyser.getByteFrequencyData(dataArray);
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        const width = canvas.width;
+        const height = canvas.height;
+
+        ctx.fillStyle = '#A8C0FF';
+        ctx.beginPath();
+
+        const sliceWidth = width / bufferLength;
+        let x = 0;
+
+        for (let i = 0; i < bufferLength; i++) {
+            const v = dataArray[i] / 255;
+            const y = height - (v * height);
+
+            if (i === 0) {
+                ctx.moveTo(x, y);
+            } else {
+                ctx.lineTo(x, y);
+            }
+
+            x += sliceWidth;
+        }
+
+        ctx.lineTo(width, height);
+        ctx.lineTo(0, height);
+        ctx.closePath();
+        ctx.fill();
+    }
+
+    draw();
+}
 
 async function startRecording() {
     if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
@@ -35,8 +90,9 @@ async function startRecording() {
         analyser = audioContext.createAnalyser();
         analyser.fftSize = 2048;
         source.connect(analyser);
+        drawWaveform()
 
-        recordButton.textContent = '■ Stop';
+        recordButton.querySelector('.record-label').textContent = '■ Stop';
         isRecording = true;
     } catch (err) {
         alert("Microphone access is required to record audio.");
@@ -54,7 +110,15 @@ function stopRecording() {
     }
 
     isRecording = false;
-    recordButton.textContent = '● Record';
+    recordButton.querySelector('.record-label').textContent = '● Record';
+
+    if (animationId) {
+        cancelAnimationFrame(animationId);
+        animationId = null;
+    }
+    if (canvasCtx && canvas) {
+        canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+    }
 }
 
 function saveRecording() {
